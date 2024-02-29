b96a11497ba4c182fb8e93a258635b251938207b
Sahusuman880 <sahusuman880@gmail.com>
2024-02-29 18:17:46 +0530

    Adding all initial files for the App

---
+++ public/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
